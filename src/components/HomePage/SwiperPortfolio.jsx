7c0c714b6e41597df57396b1082653a91135ce91
paussaanz <paula@sanz.eu>
2024-09-06 13:38:18 +0200

    cambios swiper horizontal

--- src/components/HomePage/SwiperPortfolio.jsx
+++ src/components/HomePage/SwiperPortfolio.jsx
@@ -23,30 +23,52 @@
 //     );
 // }
 
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import ImageBanner from "./ImageBanner";
 import { homeSwiperImages } from '../../assets/js/images';
 
-const SwiperPortfolio = () => {
+const SwiperPortfolio = ({ inverseScale }) => {
     const ref = useRef(null);
+    const [canScroll, setCanScroll] = useState(false);
 
-  const {scrollYProgress} = useScroll({ target: ref});
+    const { scrollYProgress } = useScroll({ target: ref });
 
-  const x = useTransform(scrollYProgress, [0,1], ['0%', '-60%'])
+    // Definimos useTransform sin condicionales
+    const x = useTransform(scrollYProgress, [0, 1], ['0%', '-60%']);
+
+    useEffect(() => {
+        console.log("CURRENT", inverseScale);
+        // Comprobamos el valor de inverseScale.current en lugar de inverseScale
+        if (inverseScale === 1) {
+            setCanScroll(true);
+        } else {
+            // setCanScroll(false);
+        }
+    }, [inverseScale]);
+
     return (
         <section ref={ref} className="relative vh-300 bg-transparent">
-            <div className=" position-sticky top-0 d-flex vh-100 align-items-center justify-content-start overflow-hidden ">
+            <div className="position-sticky top-0 d-flex vh-100 align-items-center justify-content-start overflow-hidden">
                 
-                    <motion.div className="swiper-images " style={{x}}>
-                        {homeSwiperImages.map((image, index) => (
-                            <ImageBanner key={index} src={image.src} name={image.name} date={image.date} description={image.description} />
-                        ))}
-                    </motion.div>
-                
+                <motion.div 
+                    className="swiper-images" 
+                    // Si canScroll es false, x es '0%'
+                    style={{ x: canScroll ? x : '0%' }}  
+                >
+                    {homeSwiperImages.map((image, index) => (
+                        <ImageBanner 
+                            key={index} 
+                            src={image.src} 
+                            name={image.name} 
+                            date={image.date} 
+                            description={image.description} 
+                        />
+                    ))}
+                </motion.div>
             </div>
         </section>
     );
-}
+};
 
 export default SwiperPortfolio;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import VideoBackground from '../components/HomePage/VideoBackground';
 import TextOverlay from '../components/General/TextOverlay';
 import Button from '../components/General/Buttons/Button';
@@ -7,59 +7,45 @@ import Cursor from '../components/Cursor/Cursor';
 import { useScroll, useTransform, motion } from 'framer-motion';
 import TextAnimationContainer from '../components/General/TextAnimationContainer';
 
+
 const HomePage = () => {
     const homepage = useRef();
-    const [isSwiperActive, setIsSwiperActive] = useState(false); // Estado para activar swiper
     const { scrollYProgress } = useScroll({
-        target: homepage,
-        offset: ["start start", "end end"]
-    });
+            target: homepage,
+            offset: ["start start", "end end"]
+    })
 
-    // Animaciones de escala y rotación
-    const scale = useTransform(scrollYProgress, [0.5, 1], [1, 0.8]);
-    const rotate = useTransform(scrollYProgress, [0.5, 1], [0, -10]);
+    const scale = useTransform(scrollYProgress, [ 0.5 , 1 ], [ 1 , 0.8 ])
+    const rotate = useTransform(scrollYProgress, [ 0.5 , 1 ], [ 0 , -5 ])
     
     const inverseScale = useTransform(scrollYProgress, [0.5, 1], [0.8, 1]);
     const inverseRotate = useTransform(scrollYProgress, [0.5, 1], [5, 0]);
 
-    // Monitorear el valor de inverseScale
-    useEffect(() => {
-        const unsubscribe = inverseScale.onChange((latest) => {
-            if (latest === 1) {
-                setIsSwiperActive(true); // Activa el swiper cuando inverseScale es 1
-            } else {
-                setIsSwiperActive(false); // Desactiva el swiper si inverseScale es diferente de 1
-            }
-        });
-
-        return () => unsubscribe(); // Limpieza cuando el componente se desmonta
-    }, [inverseScale]);
+    console.log(inverseScale)
 
     return (
-        <div className="homepage position-relative" ref={homepage}>
+        <div className="homepage position-relative ">
             <Cursor />
             <section className="hero-homepage position-relative">
                 <VideoBackground videoSrc="/DJI_0155.MP4" height="vh-100" />
                 <TextOverlay textColor="text-light" textPosition="text-center" className="text-animated">
                     <h1>
                         <span className='d-block'>Capture</span>
-                        <span className="h2 d-block">Brilliance</span>
+                        <span className="h2 d-block">Brilliance </span>
                     </h1>
                     <Button href="/portfolio" text="See my work" className="text-light" />
                 </TextOverlay>
             </section>
-
-            <motion.div style={{ scale, rotate }} className="bg-light position-sticky top-0 py-5 vh-100 align-content-center" scrollyprogress={scrollYProgress}>
-                <TextAnimationContainer text="Photography is the art of capturing fleeting moments, turning the transient into something eternal. Through the lens, everyday scenes transform into extraordinary glimpses of life. It allows us to explore perspectives beyond our own, revealing the hidden depths of both nature and humanity." />
+            <motion.div style={{scale, rotate}} className="bg-light position-sticky top-0 py-5 vh-100 align-content-center" scrollyprogress={scrollYProgress}>
+                <TextAnimationContainer  text=" Photography is the art of capturing fleeting moments, turning the transient into something eternal.
+                    Through the lens, everyday scenes transform into extraordinary glimpses of life.
+                    It allows us to explore perspectives beyond our own, revealing the hidden depths of both nature and humanity." />
             </motion.div>
-
-            <motion.div style={{ scale: inverseScale, rotate: inverseRotate }} scrollyprogress={scrollYProgress} className="position-relative text-center">
+            <motion.div style={{scale: inverseScale,rotate: inverseRotate}} scrollyprogress={scrollYProgress} className="position-relative text-center" >
                 <div className="position-absolute z-3 text-light text-center centered-button">
                     <Button className="text-light" text="Portfolio" /> | <Button className="text-light" text="Editorials" />
                 </div>
-
-                {/* Pasamos el estado de isSwiperActive al SwiperPortfolio */}
-                <SwiperPortfolio isActive={isSwiperActive} />
+                <SwiperPortfolio inverseScale={inverseScale.current}/>
             </motion.div>
         </div>
     );
