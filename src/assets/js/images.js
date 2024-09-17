a0cac5aaabf09f4e191428784ef4b0fe133c865e
paussaanz <paula@sanz.eu>
2024-09-17 18:35:02 +0200

    image js

--- src/assets/js/images.js
+++ src/assets/js/images.js
@@ -1,7 +1,11 @@
-import NatureHome from '/CR1.jpg';
-import PhotoshootsHome from '/Diphda1.jpg';
-import SportsHome from '/RM1.jpg';
-
+import NatureHome from '/images/nature-1.jpg';
+import NaturePortfolio from '/images/nature-2.jpg';
+import PhotoshootsHome from '/images/photoshoots-1.jpg';
+import PhotoshootsPortfolio from '/images/photoshoots-2.jpg';
+import SportsHome from '/images/sports-1.jpg';
+import SportsPortfolio from '/images/sports-2.jpg';
+import MusicPortfolio from '/images/music-1.jpg';
+import LifestylePortfolio from '/images/lifestyle-1.jpg';
 
 export const homeSwiperImages = [
     {   src : NatureHome,
@@ -33,5 +37,34 @@ export const portfolioParallaxHero = [
     },
     {   src : SportsHome,
         y: "lg"
+    },
+];
+
+export const portfolioCardAnimation = [
+    {   src : PhotoshootsPortfolio,
+        name : "Photoshoots",
+        date : 2023,
+        description : "Description 2"
+    }
+    ,
+    {   src : NaturePortfolio,
+        name : "Nature",
+        date : 2023,
+        description : "Description 1"
+    },
+    {   src : SportsPortfolio,
+        name : "Sports",
+        date : 2023,
+        description : "Description 3"
+    },
+    {   src : MusicPortfolio,
+        name : "Music",
+        date : 2023,
+        description : "Description 3"
+    },
+    {   src : LifestylePortfolio,
+        name : "Lifestyle",
+        date : 2023,
+        description : "Description 3"
     },
 ];
