1c74608572ba103ecd42e919950e3dfa8fe19e16
paussaanz <paula@sanz.eu>
2024-09-18 11:08:59 +0200

    editorials page hero

---
+++ src/components/EditorialsPage/HeroEditorials.jsx
@@ -0,0 +1,48 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import ParallaxImages from "../General/ParallaxImages";
+
+const HeroEditorials = ({images = []}) => {
+    const container = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ['start end', 'end start']
+    });
+
+    const xsm = useTransform(scrollYProgress, [0, 1], [0, 180]);
+    const sm = useTransform(scrollYProgress, [0, 1], [0, 820]);
+    const md = useTransform(scrollYProgress, [0, 1], [0, -450]);
+    const lg = useTransform(scrollYProgress, [0, 1], [0, -600]);
+
+    const getYTransform = (size) => {
+        switch (size) {
+            case 'sm':
+                return sm;
+            case 'md':
+                return md;
+            case 'lg':
+                return lg;
+            default:
+                return 0;
+        }
+    };
+
+    return (
+        <>
+            <div ref={container} className="container hero-cabecera-editorials">
+            <div className="images-3 position-relative d-flex w-100 justify-content-center">
+                <ParallaxImages images={images} getYTransform={getYTransform} containerNumber={3}/>
+            </div>
+            <div className="body text-primary text-center d-flex justify-content-center align-items-end vh-100">
+                <motion.h1 style={{ y: xsm }} className="mb-5 text-uppercase text-primary">
+                    EDITORIALS
+                </motion.h1>
+            </div>
+
+            
+        </div>
+        </>
+    );
+};
+
+export default HeroEditorials;
