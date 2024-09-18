8d044df799c5b41a0f12dd8e646cabbb60c79f3d
paussaanz <paula@sanz.eu>
2024-09-18 11:06:56 +0200

    project details created

--- src/pages/PortfolioDetailPage.jsx
+++ src/pages/PortfolioDetailPage.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { editorialsParallaxHero } from "../assets/js/images";
+import HeroDetails from "../components/EditorialsDetailsPage/HeroDetails";
+import TextAnimationContainer from "../components/General/TextAnimationContainer";
 
 const PortfolioDetailPage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <section className="hero-details position-relative">
+                <HeroDetails src="/images/photoshoots-4.jpg" />
+            </section>
+            <section className="text-animation">
+                <div className="py-5 vh-100 align-content-center">
+                    <TextAnimationContainer
+                        text="Photography transforms ordinary moments into lasting memories, capturing the beauty and uniqueness of every scene. My portfolio is a journey through the lens, showcasing diverse perspectives and intimate glimpses of life. Each image is a story, a testament to the power of visual storytelling. This collection celebrates the art of seeing, from serene landscapes to vibrant street scenes."
+                        textColor='text-light'
+                        maskColor="bg-primary"
+                    />
+                </div>
+            </section>
+        </>
     );
 };
 
