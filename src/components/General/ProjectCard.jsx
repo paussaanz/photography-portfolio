c84c1c1f0fe14f803e8c053a42288efa06757ca4
paussaanz <paula@sanz.eu>
2024-09-23 12:07:08 +0200

    space changes

--- src/components/General/ProjectCard.jsx
+++ src/components/General/ProjectCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const ProjectCard = ({src, name, date, description, orientation = 'vertical'}) => {
+const ProjectCard = ({ src, name, date, description, orientation = 'vertical' }) => {
 
     const isHorizontal = orientation === 'horizontal';
 
     return (
         <>
             <div className="d-flex align-items-center justify-content-center flex-column">
-                <div className={`${isHorizontal ? 'aspect-16-9 ' : 'aspect-box'} d-flex flex-column justify-content-center align-items-center py-3`} style={{height: '50vh', width: '50vw'}}>
+                <div className={`${isHorizontal ? 'aspect-16-9 ' : 'aspect-box'} d-flex flex-column justify-content-center align-items-center py-3`} style={{ height: '50vh', width: '50vw' }}>
                     <img className="h-100 w-100 object-fit-cover" src={src} alt="" />
                 </div>
 
