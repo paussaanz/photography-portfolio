d7cd9a83743b520b3a6f25a03560c92f5b8fba0a
paussaanz <paula@sanz.eu>
2024-09-23 12:01:16 +0200

    images imports

--- src/assets/js/images.js
+++ src/assets/js/images.js
@@ -10,108 +10,420 @@ import EditorialCover1 from '/images/editorial-cover-1.png';
 import EditorialCover2 from '/images/editorial-cover-2.png';
 
 //GENERALES
+import Photoshoots1 from '/images/photoshoots-1.jpg';
+import Photoshoots2 from '/images/photoshoots-2.jpg';
 import Photoshoots3 from '/images/photoshoots-3.jpg';
 import Photoshoots4 from '/images/photoshoots-4.jpg';
 import Photoshoots5 from '/images/photoshoots-5.jpg';
 import Photoshoots6 from '/images/photoshoots-6.jpg';
+import Photoshoots7 from '/images/photoshoots-7.jpg';
+import Photoshoots8 from '/images/photoshoots-8.jpg';
+import Photoshoots9 from '/images/photoshoots-9.jpg';
+import Photoshoots10 from '/images/photoshoots-10.jpg';
+import Photoshoots11 from '/images/photoshoots-11.jpg';
+import Photoshoots12 from '/images/photoshoots-12.jpg';
+import Photoshoots13 from '/images/photoshoots-13.jpg';
+import Photoshoots14 from '/images/photoshoots-14.jpg';
+import Photoshoots15 from '/images/photoshoots-15.jpg';
+import Photoshoots16 from '/images/photoshoots-16.jpg';
+import Photoshoots17 from '/images/photoshoots-17.jpg';
+import Photoshoots18 from '/images/photoshoots-18.jpg';
+import Photoshoots19 from '/images/photoshoots-19.jpg';
+import Photoshoots20 from '/images/photoshoots-20.jpg';
+import Photoshoots21 from '/images/photoshoots-21.jpg';
+import Photoshoots22 from '/images/photoshoots-22.jpg';
+import Photoshoots23 from '/images/photoshoots-23.jpg';
+import Photoshoots24 from '/images/photoshoots-24.jpg';
+import Photoshoots25 from '/images/photoshoots-25.jpg';
+import Photoshoots26 from '/images/photoshoots-26.jpg';
+import Photoshoots27 from '/images/photoshoots-27.jpg';
+import Photoshoots28 from '/images/photoshoots-28.jpg';
+import Photoshoots29 from '/images/photoshoots-29.jpg';
+import Photoshoots30 from '/images/photoshoots-30.jpg';
+import Photoshoots31 from '/images/photoshoots-31.jpg';
+import Photoshoots32 from '/images/photoshoots-32.jpg';
+import Photoshoots33 from '/images/photoshoots-33.jpg';
+
+import Music2 from '/images/music-2.jpg';
+import Music3 from '/images/music-3.jpg';
+import Music4 from '/images/music-4.jpg';
+import Music5 from '/images/music-5.jpg';
+import Music6 from '/images/music-6.jpg';
+import Music7 from '/images/music-7.jpg';
 
 
 
 export const homeSwiperImages = [
-    {   src : NatureHome,
-        name : "Nature",
-        date : 2023,
-        description : "Description 1"
+    {
+        src: NatureHome,
+        name: "Nature",
+        date: 2023,
+        description: "Description 1"
     }
     ,
-    {   src : PhotoshootsHome,
-        name : "Photoshoots",
-        date : 2023,
-        description : "Description 2"
+    {
+        src: PhotoshootsHome,
+        name: "Photoshoots",
+        date: 2023,
+        description: "Description 2"
     },
-    {   src : SportsHome,
-        name : "Sports",
-        date : 2023,
-        description : "Description 3"
+    {
+        src: SportsHome,
+        name: "Sports",
+        date: 2023,
+        description: "Description 3"
     }
 ];
 
 
 export const portfolioParallaxHero = [
-    {   src : NatureHome,
-        y : "sm"
+    {
+        src: NatureHome,
+        y: "sm"
     }
     ,
-    {   src : PhotoshootsHome,
+    {
+        src: PhotoshootsHome,
         y: "md",
     },
-    {   src : SportsHome,
+    {
+        src: SportsHome,
         y: "lg"
     },
 ];
 
 export const portfolioCardAnimation = [
-    {   src : PhotoshootsPortfolio,
-        name : "Photoshoots",
-        date : 2023,
-        description : "Description 2",
-        url : "/portfolio/photoshoots"
+    {
+        src: PhotoshootsPortfolio,
+        name: "Photoshoots",
+        date: 2023,
+        description: "Description 2",
+        url: "/portfolio/photoshoots"
     }
     ,
-    {   src : NaturePortfolio,
-        name : "Nature",
-        date : 2023,
-        description : "Description 1",
-        url : "/portfolio/nature"
+    {
+        src: NaturePortfolio,
+        name: "Nature",
+        date: 2023,
+        description: "Description 1",
+        url: "/portfolio/nature"
     },
-    {   src : SportsPortfolio,
-        name : "Sports",
-        date : 2023,
-        description : "Description 3",
-        url : "/portfolio/sports"
+    {
+        src: SportsPortfolio,
+        name: "Sports",
+        date: 2023,
+        description: "Description 3",
+        url: "/portfolio/sports"
     },
-    {   src : MusicPortfolio,
-        name : "Music",
-        date : 2023,
-        description : "Description 3",
-        url : "/portfolio/music"
+    {
+        src: MusicPortfolio,
+        name: "Music",
+        date: 2023,
+        description: "Description 3",
+        url: "/portfolio/music"
     },
-    {   src : LifestylePortfolio,
-        name : "Lifestyle",
-        date : 2023,
-        description : "Description 3",
-        url : "/portfolio/lifestyle"
+    {
+        src: LifestylePortfolio,
+        name: "Lifestyle",
+        date: 2023,
+        description: "Description 3",
+        url: "/portfolio/lifestyle"
     },
 ];
 
 
 export const editorialsParallaxHero = [
-    {   src : Photoshoots3,
-        y : "sm"
+    {
+        src: Music7,
+        y: "sm"
     }
     ,
-    {   src : Photoshoots4,
+    {
+        src: Music6,
         y: "md",
     },
-    {   src : Photoshoots5,
+    {
+        src: Music2,
         y: "lg"
     },
-    {   src : Photoshoots6,
+    {
+        src: Music3,
         y: "sm"
     },
-   
+    {
+        src: Music4,
+        y: "sm"
+    },
+
 ];
 
 
 export const editorialsCovers = [
-    {   src : EditorialCover1,
-        name : "GREECE",
-        year : "2022"
+    {
+        src: EditorialCover1,
+        name: "GREECE",
+        year: "2022"
     }
     ,
-    {   src : EditorialCover2,
+    {
+        src: EditorialCover2,
         name: "TANZANIA",
-        year : "2023"
+        year: "2023"
     },
-   
+    {
+        src: EditorialCover2,
+        name: "TANZANIA",
+        year: "2023"
+    },
+    {
+        src: EditorialCover1,
+        name: "GREECE",
+        year: "2022"
+    }
+    ,
+    {
+        src: EditorialCover1,
+        name: "GREECE",
+        year: "2022"
+    }
+    ,
 ];
+
+export const photoshootsDetails = [
+    {
+        src: Photoshoots1,
+        colStart: 1,
+        colSpan: 4,
+        rowStart: 1,
+        rowSpan: 6,
+        size: "100%"
+    },
+    {
+        src: Photoshoots2,
+        colStart: 8,
+        colSpan: 4,
+        rowStart: 1,
+        rowSpan: 6,
+        height: "70%",
+        width: "85%"
+    },
+    {
+        src: Photoshoots3,
+        colStart: 3,
+        colSpan: 4,
+        rowStart: 7,
+        rowSpan: 7,
+        height: "85%", 
+        width: "100%"
+    },
+    {
+        src: Photoshoots4,
+        colStart: 8,
+        colSpan: 4,
+        rowStart: 7,
+        rowSpan: 6,
+    },
+    {
+        src: Photoshoots5,
+        colStart: 2,
+        colSpan: 6,
+        rowStart: 14,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots6,
+        colStart: 9,
+        colSpan: 4,
+        rowStart: 15,
+        rowSpan: 7,
+    },
+    {
+        src: Photoshoots7,
+        colStart: 1,
+        colSpan: 4,
+        rowStart: 20,
+        rowSpan: 6,
+    },
+    {
+        src: Photoshoots8,
+        colStart: 6,
+        colSpan: 6,
+        rowStart: 23,
+        rowSpan: 4,
+    },
+    {
+        src: Photoshoots9,
+        colStart: 3,
+        colSpan: 4,
+        rowStart: 28,
+        rowSpan: 6,
+    },
+    {
+        src: Photoshoots10,
+        colStart: 8,
+        colSpan: 4,
+        rowStart: 29,
+        rowSpan: 3,
+    },
+    {
+        src: Photoshoots11,
+        colStart: 8,
+        colSpan: 4,
+        rowStart: 34,
+        rowSpan: 6,
+    },
+    {
+        src: Photoshoots12,
+        colStart: 2,
+        colSpan: 4,
+        rowStart: 35,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots13,
+        colStart: 1,
+        colSpan: 6,
+        rowStart: 41,
+        rowSpan: 9,
+    },
+    {
+        src: Photoshoots14,
+        colStart: 9,
+        colSpan: 3,
+        rowStart: 41,
+        rowSpan: 4,
+    },
+    {
+        src: Photoshoots15,
+        colStart: 8,
+        colSpan: 5,
+        rowStart: 47,
+        rowSpan: 10,
+    },
+    {
+        src: Photoshoots16,
+        colStart: 3,
+        colSpan: 3,
+        rowStart: 51,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots17,
+        colStart: 9,
+        colSpan: 3,
+        rowStart: 58,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots18,
+        colStart: 2,
+        colSpan: 4,
+        rowStart: 57,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots19,
+        colStart: 3,
+        colSpan: 3,
+        rowStart: 63,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots20,
+        colStart: 7,
+        colSpan: 3,
+        rowStart: 64,
+        rowSpan: 6,
+    },
+    {
+        src: Photoshoots21,
+        colStart: 4,
+        colSpan: 4,
+        rowStart: 69,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots22,
+        colStart: 9,
+        colSpan: 3,
+        rowStart: 71,
+        rowSpan: 6,
+    },
+    {
+        src: Photoshoots23,
+        colStart: 2,
+        colSpan: 5,
+        rowStart: 75,
+        rowSpan: 3,
+    },
+    {
+        src: Photoshoots24,
+        colStart: 8,
+        colSpan: 4,
+        rowStart: 78,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots25,
+        colStart: 2,
+        colSpan: 3,
+        rowStart: 79,
+        rowSpan: 4,
+    },
+    {
+        src: Photoshoots26,
+        colStart: 4,
+        colSpan: 3,
+        rowStart: 82,
+        rowSpan: 4,
+    },
+    {
+        src: Photoshoots27,
+        colStart: 2,
+        colSpan: 4,
+        rowStart: 87,
+        rowSpan: 3,
+    },
+    {
+        src: Photoshoots28,
+        colStart: 8,
+        colSpan: 3,
+        rowStart: 84,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots29,
+        colStart: 2,
+        colSpan: 10,
+        rowStart: 91,
+        rowSpan: 5,
+    },
+    {
+        src: Photoshoots30,
+        colStart: 2,
+        colSpan: 3,
+        rowStart: 97,
+        rowSpan: 4,
+    },
+    {
+        src: Photoshoots31,
+        colStart: 8,
+        colSpan: 5,
+        rowStart: 97,
+        rowSpan: 8,
+    },
+    {
+        src: Photoshoots32,
+        colStart: 2,
+        colSpan: 5,
+        rowStart: 102,
+        rowSpan: 3,
+    },
+    {
+        src: Photoshoots33,
+        colStart: 1,
+        colSpan: 4,
+        rowStart: 106,
+        rowSpan: 7,
+    },
+]
