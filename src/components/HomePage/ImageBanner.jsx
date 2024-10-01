d94a9c7160312fb99c40506b2dc4dafddda2a7eb
paussaanz <paula@sanz.eu>
2024-10-01 18:47:27 +0200

    image banner changes

--- src/components/HomePage/ImageBanner.jsx
+++ src/components/HomePage/ImageBanner.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ProjectCard from '../General/ProjectCard';
 
-const ImageBanner = ({ src, description, date, name }) => {
+const ImageBanner = ({ src, description, date, name, url }) => {
     return (
-        <div className="position-relative vh-100 vw-100">
-            <div className="blur overdark position-absolute" style={{ backgroundImage: `url(${src})` }}></div>
-            <div className="container h-100 d-flex justify-content-center align-items-center">
-                <ProjectCard src={src} name={name} description={description} date={date} orientation="horizontal"/>
+        <a href={url} className="text-decoration-none">
+            <div className="position-relative vh-100 vw-100">
+                <div className="blur overdark position-absolute" style={{ backgroundImage: `url(${src})` }}></div>
+                <div className="container h-100 d-flex justify-content-center align-items-center">
+                    <ProjectCard src={src} name={name} description={description} date={date} orientation="horizontal" />
+                </div>
             </div>
-        </div>
+        </a>
     );
 };
 
