7c8f2077f1f5a96a7de7294817f525d22db7a81a
paussaanz <paula@sanz.eu>
2024-10-01 18:44:59 +0200

    image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteImagemin from 'vite-plugin-imagemin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteImagemin({
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 75,
+      },
+    }),
+  ],
+});
