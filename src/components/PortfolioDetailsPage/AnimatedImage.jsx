fea51216aa0746e95a51c3f56654c908be5200e7
paussaanz <paula@sanz.eu>
2024-10-07 23:51:42 +0200

    image mouse follow feature

--- src/components/PortfolioDetailsPage/AnimatedImage.jsx
+++ src/components/PortfolioDetailsPage/AnimatedImage.jsx
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
+import { cssNumber } from "jquery";
 
 const AnimatedImage = ({
   src,
@@ -17,12 +18,35 @@ const AnimatedImage = ({
     offset: ["start center", "center start"],
   });
 
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [transitionTime, setTransitionTime] = useState('0.5')
+
+
   // Define una sola transformación para el paralaje
   const yTransform = useTransform(scrollYProgress, [0, 1], [0, parallaxSpeed]);
   const scaleTransform = useTransform(scrollYProgress, [0, 1], [1, 1.3]); // Escalado de 1 a 1.2
 
+  const handleMouseMove = (e) => {
+
+    const rect = container.current.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setMousePosition({ x, y });
+  setTimeout(() => {
+    setTransitionTime('none')
+  }, 1000 )
+
+  };
+
+  const handleMouseLeave = () => {
+    setMousePosition({ x: 0, y: 0 })
+  }
+
+
   return (
     <div
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
       ref={container}
       style={{
         gridColumn: `${colStart} / span ${colSpan}`,
@@ -38,7 +62,10 @@ const AnimatedImage = ({
         alt="Project Image"
         initial={{ scale: 1 }}
         style={{
-          translateY: yTransform, // Aplicar el efecto de paralaje
+          translateY: yTransform,
+          marginTop: `-${mousePosition.y * 0.05}px`, // Aplicar el efecto de paralaje
+          marginLeft: `-${mousePosition.x *  0.05}px`, 
+          transition: transitionTime + 's',
           scale: scaleTransform, // Aplicar el escalado
           objectFit: "cover",
           width: "100%",
