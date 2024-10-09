a5d785830cb6ed70ea8fc1d9c37d8ba5b3349dee
paussaanz <paula@sanz.eu>
2024-10-09 17:41:51 +0200

    gallery view

---
+++ src/components/PortfolioDetailsPage/ImageGallery.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { portfolioDetailsGallery } from "../../assets/js/images";
+import { motion, useTransform } from "framer-motion";
+import GalleryItem from "./GalleryItem";
+
+const ImageGallery = ({images}) => {
+
+    function groupImages(images, groupSize) {
+        const groupedImages = [];
+      
+        for (let i = 0; i < images.length; i += groupSize) {
+          const group = images.slice(i, i + groupSize);
+          groupedImages.push(group);
+        }
+      
+        return groupedImages;
+      }
+      
+
+    const [items, setItems] = useState(groupImages(images, 6));
+    const [mousePositions, setMousePositions] = useState({});
+
+
+    
+    
+
+    const handleMouseMoveImg = (e, itemId) => {
+        const { clientX, clientY, currentTarget } = e;
+        const rect = currentTarget.getBoundingClientRect();
+        const x = clientX - rect.left; // Mouse position relative to the image
+        const y = clientY - rect.top;
+
+        setMousePositions((prevPositions) => ({
+            ...prevPositions,
+            [itemId]: { x, y }, // Update position for the specific item
+        }));
+    };
+
+    const handleMouseLeaveImg = (itemId) => {
+        setMousePositions((prevPositions) => ({
+            ...prevPositions,
+            [itemId]: null, // Reset the mouse position on leave
+
+        }));
+    };
+
+   
+    return (
+        <>
+                    {items.map((row, rowIndex) => (
+                        <div className="row" key={`row-${rowIndex}`}>
+                            {row.map((item) => (
+                                <GalleryItem item={item} mousePositions={mousePositions} handleMouseLeaveImg={handleMouseLeaveImg} handleMouseMoveImg={handleMouseMoveImg}/>
+                            ))}
+                        </div>
+                    ))}
+
+        </>
+    );
+};
+
+export default ImageGallery;
--- src/pages/PortfolioDetailPage.jsx
+++ src/pages/PortfolioDetailPage.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import AnimatedImage from "../components/PortfolioDetailsPage/AnimatedImage";
 import HeroDetails from "../components/PortfolioDetailsPage/HeroDetails";
 import TextAnimationContainer from "../components/General/TextAnimationContainer";
-
+import Button from "../components/General/Buttons/Button";
+import ImageGallery from "../components/PortfolioDetailsPage/ImageGallery";
 
 const PortfolioDetailPage = ({ images, title, textAnimation }) => {
-    const { heroImage, projectImages } = images
+    const { heroImage, projectImages } = images;
+    const [activeContent, setActiveContent] = useState('Grid');
+    const imagesSectionRef = useRef(null);
+    const galleryRef = useRef(null);
+
+    const handleShowGrid = () => {
+        setActiveContent('Grid');
+    };
+
+    const handleShowGallery = () => {
+        setActiveContent('Gallery');
+    };
+
+    useLayoutEffect(() => {
+        if (imagesSectionRef.current) {
+            imagesSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [activeContent]);
+
+    const handleMouseMove = (e) => {
+        if (!galleryRef.current) return; // Prevents errors if gallery is not rendered
+        const { clientX, clientY, currentTarget } = e;
+        const { width, height } = currentTarget.getBoundingClientRect();
+        const centerX = width / 2;
+        const centerY = height / 2;
+
+        const sensitivityX = 0.9;
+        const sensitivityY = 0.25;
+        const deltaX = (centerX - clientX) / sensitivityX;
+        const deltaY = (centerY - clientY) / sensitivityY;
+
+        galleryRef.current.style.transform = `translate(calc(-50% + ${deltaX}px), calc(-50% + ${deltaY}px))`;
+    };
 
     return (
         <div data-barba="container" className="barba-container">
@@ -21,21 +54,39 @@ const PortfolioDetailPage = ({ images, title, textAnimation }) => {
                     />
                 </div>
             </section>
-            <section className="position-relative">
-                <div className="project-details d-grid justify-content-center align-items-center">
-                    {images && projectImages.map((img, index) => (
-                        <AnimatedImage
-                            key={index}
-                            src={img.src}
-                            colStart={img.colStart}  
-                            colSpan={img.colSpan}      
-                            rowStart={img.rowStart} 
-                            rowSpan={img.rowSpan}
-                            width={img.width}
-                            height={img.height}
-                        />
-                    ))}
+
+            <section className="images-gallery" ref={imagesSectionRef}>
+                <div className="text-dark text-center bottom-fixed-button">
+                    <Button className="text-dark" text="Grid" onClick={handleShowGrid} />
+                    |
+                    <Button className="text-dark" text="Gallery" onClick={handleShowGallery} />
                 </div>
+                {
+                    activeContent === 'Grid' ? (
+                        <div className="project-details d-grid justify-content-center align-items-center">
+                            {images && projectImages.map((img, index) => (
+                                <AnimatedImage
+                                    key={index}
+                                    src={img.src}
+                                    colStart={img.colStart}
+                                    colSpan={img.colSpan}
+                                    rowStart={img.rowStart}
+                                    rowSpan={img.rowSpan}
+                                    width={img.width}
+                                    height={img.height}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="container-gallery" onMouseMove={handleMouseMove}>
+                            <div className="gallery" ref={galleryRef}>
+                                <ImageGallery
+                                    images={projectImages}
+                                />
+                            </div>
+                        </div>
+                    )
+                }
             </section>
         </div>
     );
