a078a1939f29f4c690ae19826b75fc29fc75b7d9
paussaanz <paula@sanz.eu>
2024-10-16 22:59:04 +0200

    footer wip

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import InfiniteCarrusel from './InfiniteCarrusel';
 
 const Footer = () => {
     return (
-        <div className='vh-100 bg-primary'>
-            FOOTER
+        <div className='vh-100 d-flex flex-column bg-primary'>
+            <section className='infinite-carrusel'>
+                <InfiniteCarrusel />
+            </section>
+            <section className='3d-logo'>
+            <h1>SECCION 3D</h1>
+            <h1>SECCION 3D</h1>
+            <h1>SECCION 3D</h1>
+            </section>
+            <section className='bottom-links mt-auto'>
+                <div className="d-flex justify-content-between p-4 text-secondary text-uppercase">
+                    <p>Privacy policy</p>
+                    <p>Social Media Links</p>
+                    <p>Site by SYP</p>
+                </div>
+            </section>
         </div>
     );
 };
