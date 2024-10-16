5ca85d7b54ecc20eba115d66d4b54ad5c65fa7e5
paussaanz <paula@sanz.eu>
2024-10-16 22:58:00 +0200

    added infinte carrusel  and scss

---
+++ src/components/Footer/InfiniteCarrusel.jsx
@@ -0,0 +1,26 @@
+const InfiniteCarrusel = () => {
+    return (
+        <div className="text-banner-wrapper h-jumbo">
+            <div className="text-banner-item">
+                <div>GET IN TOUCH</div>
+                <div>
+                    <img src="/logo-dark.svg" />
+                </div>
+            </div>
+            <div className="text-banner-item">
+                <div>GET IN TOUCH</div>
+                <div>
+                    <img src="/logo-dark.svg" />
+                </div>
+            </div>
+            <div className="text-banner-item">
+                <div>GET IN TOUCH</div>
+                <div>
+                    <img src="/logo-dark.svg" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default InfiniteCarrusel;
