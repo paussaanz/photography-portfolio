2cee5d2a9ea42dd8de54a50c9965e537b4335109
paussaanz <paula@sanz.eu>
2024-10-21 23:05:12 +0200

    detail close added

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 
-const Detail = ({ images }) => {
+const Detail = ({ images, closeOverlay }) => {
 
-  
+
 
   const [selectedImage, setSelectedImage] = useState(images[0].src);
 
-  
+
   return (
     <div data-barba="container" className="gallery-container">
       <motion.div
@@ -18,44 +18,52 @@ const Detail = ({ images }) => {
         transition={{ duration: 0.5 }}
       />
 
-        <motion.div
-          className="main-image-container"
-          key={selectedImage}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          <motion.img
-            src={selectedImage}
-            alt="Selected"
-            className="main-image"
-          />
-        </motion.div>
+      <motion.div
+        className="main-image-container"
+        key={selectedImage}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <motion.img
+          src={selectedImage}
+          alt="Selected"
+          className="main-image"
+        />
+      </motion.div>
 
-        <motion.div className="scroll-container">
-          <motion.div className="thumbnails-container" animate="visible">
-            {images.map((img, index) => (
-              <motion.div
-                key={index}
-                className={`thumbnail-card ${selectedImage === img.src ? 'selected' : ''}`}
-                onHoverStart={() => setSelectedImage(img.src)}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: selectedImage === img.src ? 1 : 0.3, scale: 1 }}
-                transition={{
-                  opacity: { duration: 0.4, delay: index * 0.1 },
-                  scale: { duration: 0.4 }
-                }}
-                style={{
-                  backgroundImage: `url(${img.src})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                  backgroundRepeat: 'no-repeat'
-                }}
-              />
-            ))}
-          </motion.div>
+      <button
+        className="close-overlay-button"
+        onClick={closeOverlay}
+        aria-label="Close Overlay"
+      >
+        &times;
+      </button>
+
+      <motion.div className="scroll-container">
+        <motion.div className="thumbnails-container" animate="visible">
+          {images.map((img, index) => (
+            <motion.div
+              key={index}
+              className={`thumbnail-card ${selectedImage === img.src ? 'selected' : ''}`}
+              onHoverStart={() => setSelectedImage(img.src)}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: selectedImage === img.src ? 1 : 0.3, scale: 1 }}
+              transition={{
+                opacity: { duration: 0.4, delay: index * 0.1 },
+                scale: { duration: 0.4 }
+              }}
+              style={{
+                backgroundImage: `url(${img.src})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat'
+              }}
+            />
+          ))}
         </motion.div>
-      </div>
+      </motion.div>
+    </div>
   );
 };
 
