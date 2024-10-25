91c90ce28ef7174c11bb7b9d6d6906aca8ab25d5
paussaanz <paula@sanz.eu>
2024-10-25 23:13:24 +0200

    contacto logo 3d syp

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,9 @@
+import Scene3DContact from "../components/ContactPage/Scene3DContact";
+
 const ContactPage = () => {
     return (
         <div data-barba="container" className="dimension--vh-100 position--relative">
+            <Scene3DContact/>
         </div>
     );
 };
