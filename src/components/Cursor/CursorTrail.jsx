4548011785c2ccbffde36f146fe78bbc693ea4ac
paussaanz <144829569+paussaanz@users.noreply.github.com>
2024-12-17 10:55:12 +0100

    add big cursor on hover

--- src/components/Cursor/CursorTrail.jsx
+++ src/components/Cursor/CursorTrail.jsx
@@ -1,14 +1,17 @@
-import { useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const CursorTrail = () => {
     const svgRef = useRef(null);
+    const hoverRef = useRef(null);
     const pathRef = useRef(null);
     const points = useRef([]);
     const segments = 50;
     const mouse = useRef({ x: 0, y: 0 });
+    const [hoveredLink, setHoveredLink] = useState(false);
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+    const [isExiting, setIsExiting] = useState(false);
 
     useEffect(() => {
-        // Initialize points to prevent empty array issues
         const initializePoints = () => {
             points.current = Array.from({ length: segments }, () => ({
                 x: mouse.current.x,
@@ -16,10 +19,11 @@ const CursorTrail = () => {
             }));
         };
 
-        // Handle mouse movement to update mouse position
         const handleMouseMove = (event) => {
             mouse.current.x = event.clientX;
             mouse.current.y = event.clientY;
+
+            setCursorPosition({ x: event.clientX, y: event.clientY });
 
             if (points.current.length === 0) {
                 initializePoints();
@@ -76,13 +80,55 @@ const CursorTrail = () => {
         animate();
     }, []);
 
+    useEffect(() => {
+        const handleMouseOver = (event) => {
+            if (event.target.tagName.toLowerCase() === 'a') {
+                setIsExiting(false)
+                setHoveredLink(true);
+            }
+        };
+
+        const handleMouseOut = (event) => {
+            if (event.target.tagName.toLowerCase() === 'a') {
+                setHoveredLink(false);
+                setIsExiting(true)
+            }
+        };
+
+        document.addEventListener('mouseover', handleMouseOver);
+        document.addEventListener('mouseout', handleMouseOut);
+
+        return () => {
+            document.removeEventListener('mouseover', handleMouseOver);
+            document.removeEventListener('mouseout', handleMouseOut);
+        };
+    }, []);
+
     return (
-        <svg ref={svgRef} className="cursor__trail cursor__trail--inverted"
-            style={{ willChange: 'transform' }}>
-            <path ref={pathRef} className="cursor__path" d="" />
-        </svg>
+        <>
+            {/* Cuadrado rojo que sigue al cursor */}
+            <div
+                className={`cursor__trail--hover cursor__trail--${hoveredLink ? 'hovering' : ''}  cursor__trail--${isExiting ? 'exiting' : ''}`}
+                ref={hoverRef}
+                style={{
+                    transform: `translate(${cursorPosition.x}px, ${cursorPosition.y}px) translate(-50%, -50%)`,
+                }}
+            ></div>
+
+            {/* Cursor Trail */}
+            <svg
+                ref={svgRef}
+                className={`cursor__trail cursor__trail--inverted`}
+                style={{ willChange: 'transform' }}
+            >
+                <path
+                    ref={pathRef}
+                    className={`cursor__path ${hoveredLink ? 'cursor__path--hovering' : ''}`}
+                    d=""
+                />
+            </svg>
+        </>
     );
 };
 
 export default CursorTrail;
-
