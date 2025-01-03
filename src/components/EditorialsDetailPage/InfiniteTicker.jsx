252f03349b0a4f0bbd2779ded3bab94c7fd5ed4d
Cdelatorre <carlosdelatorrediaz@gmail.com>
2025-01-04 00:39:57 +0100

    main feature

--- src/components/EditorialsDetailPage/InfiniteTicker.jsx
+++ src/components/EditorialsDetailPage/InfiniteTicker.jsx
@@ -1,25 +1,29 @@
 const InfiniteTicker = () => {
     const footerItems = Array.from({ length: 10 }, () => ({
-        text:"SYP! EDITORIALS",
+        text: "SYP! EDITORIALS",
     }));
 
     return (
         <div className="editorials-detail__ticker-container">
             <div className={`editorials-detail__ticker-wrapper editorials-detail__ticker-wrapper--top h1 text-decoration--none`}>
                 {footerItems.map((item, index) => (
-                    <div className="editorials-detail__ticker-item" key={index}>
+                    <div className="editorials-detail__ticker-item" key={423424 + index}>
                         <div className="editorials-detail__ticker-item--text">{item.text}</div>
                     </div>
                 ))}
             </div>
-            <div className={`editorials-detail__ticker-wrapper editorials-detail__ticker-wrapper--bottom h1 text-decoration--none`}>
+
+            <div style={{
+                transform: 'rotate(358deg)',
+                top: '0%',
+            }} className={`editorials-detail__ticker-wrapper editorials-detail__ticker-wrapper--top h1 text-decoration--none`}>
                 {footerItems.map((item, index) => (
-                    <div className="editorials-detail__ticker-item" key={index}>
+                    <div className="editorials-detail__ticker-item" key={1212 + index}>
                         <div className="editorials-detail__ticker-item--text">{item.text}</div>
                     </div>
                 ))}
             </div>
-        </div>
+        </div >
     );
 };
 
